1b19a0e9d589d800ffae808757c63ae3daaac9e8
zyj16602159899 <z16602159899@163.com>
2020-09-07 14:43:12 +0800

    company

--- interface/tools/read_config.py
+++ interface/tools/read_config.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python
-#-*- coding:utf-8 -*-
-#@Author：zhuxiujie
+# -*- coding:utf-8 -*-
+# @Author：zhuxiujie
 import configparser
 import os
 from tools.project_path import conf_dir
 
-config_file = os.path.join(conf_dir,'config.ini')
+config_file = os.path.join(conf_dir, 'config.ini')
+
 
 class ReadConfig:
     """封装读取和写入config文件的方法"""
 
     def __init__(self):
         self.conf = configparser.ConfigParser()
-        self.conf.read(config_file,encoding='utf-8')
+        self.conf.read(config_file, encoding='utf-8')
 
-    def read_config(self,option,section):
+    def read_config(self, option, section):
         """读取配置文件数据"""
         return self.conf[option][section]
 
-    def write_back_data(self,option,section,value):
+    def write_back_data(self, option, section, value):
         """写入配置文件数据"""
-        self.conf.set(option,section,value)
-        with open(config_file,'w',encoding='utf-8') as file:
+        self.conf.set(option, section, value)
+        with open(config_file, 'w', encoding='utf-8') as file:
             self.conf.write(file)
